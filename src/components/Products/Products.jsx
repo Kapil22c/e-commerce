ab8172b688c54f28d4d0f9a40019f58a9c197f76
Kapil bavisiya <kapilbavisiya555@gmail.com>
2020-12-19 19:30:56 +0530

    updated

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -23,15 +23,16 @@ import useStyles from "./styles";
 //   },
 // ];
 
-const Products = ({ products }) => {
+
+const Products = ( {products} ) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
-        {products.map((product) => (
+      {products.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product}  />
           </Grid>
         ))}
       </Grid>
